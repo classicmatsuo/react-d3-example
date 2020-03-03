338cdb67094bb2b408ee253ddc626b16345d2e5e
Kevin Matsuo <kmatsuo@ARDs-MacBook-Pro-2.local>
2020-03-03 16:12:36 -0600

    updated getDerivedtateFromProps function in Chart.js

--- src/visualizations/Chart.js
+++ src/visualizations/Chart.js
@@ -12,18 +12,51 @@ const colors = chroma.scale([blue, green, red]);
 
 class Chart extends Component {
   state = {
+    bars: []
   };
 
   static getDerivedStateFromProps(nextProps, prevState) {
     const {data} = nextProps;
     if (!data) return {};
-
-    return {};
+  // map date to x pos
+      // get min and max of date
+      const extent = d3.extent(data, d => d.date);
+      const xScale = d3.scaleTime()
+        .domain(extent)
+        .range([0, width])
+      // map high temp to y pos
+      // get min/max of high temp
+      const [min, max] = d3.extent(data, d => d.high);
+      const yScale = d3.scaleLinear()
+        .domain([Math.min(min, 0), max])
+        .range([height, 0])
+      
+      //map avg temp to color
+      const colorExtent = d3.extent(data, d => d.avg).reverse()
+      const colorScale = d3.scaleSequential()
+        .domain(colorExtent)
+        .interpolator(d3.interpolateRdYlBu)
+      
+      // array of objects: x, y, height
+       const bars = data.map(d => {
+         return {
+          x: xScale(d.date),
+          y: yScale(d.high),
+          height: yScale(d.low) - yScale(d.high),
+          fill: colorScale(d.avg)
+        }
+      })
+    return {bars};
   }
 
   render() {
     return (
       <svg width={width} height={height}>
+        {
+          this.state.bars.map(d => 
+            <rect x={d.x} y={d.y} width={2} height={d.height} />
+          )
+        }
       </svg>
     );
   }
