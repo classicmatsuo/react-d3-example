07774b751ab772bfab2a7402511e55f749d607bc
Kevin Matsuo <kmatsuo@ARDs-MacBook-Pro-2.local>
2020-03-04 10:34:28 -0600

    updated x and y axes in Chart.js

--- src/visualizations/Chart.js
+++ src/visualizations/Chart.js
@@ -15,21 +15,24 @@ class Chart extends Component {
     bars: []
   };
 
+  xAxis = d3.axisBottom();
+  yAxis = d3.axisLeft();
+
   static getDerivedStateFromProps(nextProps, prevState) {
     const {data} = nextProps;
     if (!data) return {};
-  // map date to x pos
+      // map date to x pos
       // get min and max of date
       const extent = d3.extent(data, d => d.date);
       const xScale = d3.scaleTime()
         .domain(extent)
-        .range([0, width])
+        .range([margin.left, width - margin.right])
       // map high temp to y pos
       // get min/max of high temp
       const [min, max] = d3.extent(data, d => d.high);
       const yScale = d3.scaleLinear()
         .domain([Math.min(min, 0), max])
-        .range([height, 0])
+        .range([height - margin.bottom, margin.top])
       
       //map avg temp to color
       const colorExtent = d3.extent(data, d => d.avg).reverse()
@@ -46,7 +49,14 @@ class Chart extends Component {
           fill: colorScale(d.avg)
         }
       })
-    return {bars};
+    return {bars, xScale, yScale};
+  }
+
+  componentDidUpdate() {
+    this.xAxis.scale(this.state.xScale);
+    d3.select(this.refs.xAxis).call(this.xAxis);
+    this.yAxis.scale(this.state.xScale);
+    d3.select(this.refs.yAxis).call(this.yAxis);
   }
 
   render() {
@@ -54,9 +64,11 @@ class Chart extends Component {
       <svg width={width} height={height}>
         {
           this.state.bars.map(d => 
-            <rect x={d.x} y={d.y} width={2} height={d.height} />
+            <rect x={d.x} y={d.y} width={2} height={d.height} fill={d.fill}/>
           )
         }
+        <g ref='xAxis' transform={`translate(0, ${height - margin.bottom})`} />
+        <g ref='yAxis' transform={`translate(${margin.left}, 0)`} />
       </svg>
     );
   }
